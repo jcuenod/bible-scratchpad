c01e6ac9ae0eb7134a8a39784a1cbf05f8e83d89
jcuenod <4253884+jcuenod@users.noreply.github.com>
2024-08-09 17:16:06 -0500

    Fix for deployments

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import bookToUsfm from "./assets/bookToUsfm.json";
 const rp = new ReferenceParser();
 import "./App.css";
 
-const API_URL = "http://localhost:3000/";
+const API_URL = import.meta.env?.VITE_API_URL ?? "http://localhost:3000/"
 
 const getVerses = async (ref) => {
   const response = await fetch(`${API_URL}?reference=${ref}`);
